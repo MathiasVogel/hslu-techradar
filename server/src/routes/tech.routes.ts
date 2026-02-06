db8af401cfb290252c626b5e9a7d75874ad64304
Mathias Vogel <mathias.vo@outlook.com>
2026-02-06 17:21:35 +0100

    feat: init openapi and swagger

--- server/src/routes/tech.routes.ts
+++ server/src/routes/tech.routes.ts
@@ -3,7 +3,29 @@ import {getTechById, getTechs} from '../controllers/tech.controller.js';
 
 const router = Router();
 
+
+/**
+ * @openapi
+ * /api/technologies:
+ * get:
+ * summary: Liste aller Technologien
+ * responses:
+ * 200:
+ * description: Erfolg
+ * content:
+ * application/json:
+ * schema:
+ * type: array
+ * items:
+ * type: object
+ * properties:
+ * id:
+ * type: string
+ * name:
+ * type: string
+ */
 router.get('/', getTechs);
+
 router.get('/:id', getTechById);
 
 export default router;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,38 @@
 import express from 'express';
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 import techRoutes from './routes/tech.routes.js';
+import path from "node:path";
 
 const app = express();
 app.use(express.json());
 
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Techradar API',
+      version: '1.0.0',
+      description: 'API Dokumentation',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: [path.join(__dirname, './routes/*.ts')],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+console.log('Anzahl gefundener Pfade:', Object.keys(swaggerDocs).length);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/api/technologies', techRoutes);
 
 app.listen(3000, () => console.log('Server runs on http://localhost:3000/api-docs'));
+
+app.get('/api-docs/json', (req, res) => {
+  res.json(swaggerDocs);
+});
