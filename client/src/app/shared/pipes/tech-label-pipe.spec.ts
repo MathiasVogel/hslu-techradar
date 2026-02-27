5c67f9f864e94f46837a1ff3c56bb7037f2f7bbf
Mathias Vogel <mathias.vo@outlook.com>
2026-02-27 19:26:08 +0100

    test: add tech-label-pipe tests

--- client/src/app/shared/pipes/tech-label-pipe.spec.ts
+++ client/src/app/shared/pipes/tech-label-pipe.spec.ts
@@ -1,8 +1,29 @@
+import { describe, it, expect, beforeEach } from 'vitest';
 import { TechLabelPipe } from './tech-label-pipe';
 
 describe('TechLabelPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TechLabelPipe();
-    expect(pipe).toBeTruthy();
+  let pipe: TechLabelPipe;
+
+  beforeEach(() => {
+    pipe = new TechLabelPipe();
+  });
+
+  it('Gibt leeren Text zurück, wenn kein Wert vorhanden ist', () => {
+    expect(pipe.transform(undefined)).toBe('');
+    expect(pipe.transform('')).toBe('');
+  });
+
+  it('wandelt Ring-Enum in lesbare Bezeichnungen um', () => {
+    expect(pipe.transform('ADOPT')).toBe('Adopt');
+    expect(pipe.transform('TRIAL')).toBe('Trial');
+    expect(pipe.transform('ASSESS')).toBe('Assess');
+    expect(pipe.transform('HOLD')).toBe('Hold');
+  });
+
+  it('wandelt Kategorie-Enum in lesbare Bezeichnungen um', () => {
+    expect(pipe.transform('TECHNIQUES')).toBe('Techniken');
+    expect(pipe.transform('LANGUAGES_AND_FRAMEWORKS')).toBe('Sprachen & Frameworks');
+    expect(pipe.transform('PLATFORMS')).toBe('Plattformen');
+    expect(pipe.transform('TOOLS')).toBe('Werkzeuge');
   });
 });
